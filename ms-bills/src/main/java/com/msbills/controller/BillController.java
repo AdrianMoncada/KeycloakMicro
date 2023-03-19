4bd9ab867cd8fd7a145f5d7ec9c21af01b09e03d
Isabela Acosta <93173449+IsabelaAcostaB@users.noreply.github.com>
2023-03-19 21:38:12 +0000

    Anotaciones de seguridad

--- ms-bills/src/main/java/com/msbills/controller/BillController.java
+++ ms-bills/src/main/java/com/msbills/controller/BillController.java
@@ -5,6 +5,7 @@ import com.msbills.service.BillService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 import java.util.List;
 
@@ -18,17 +19,20 @@ public class BillController {
 
 
   @GetMapping("/all")
+  @PreAuthorize("hasRole('ROLE_provider')")
   public ResponseEntity<List<Bill>> getAll() {
     return ResponseEntity.ok().body(service.getAllBill());
   }
 
 
   @PostMapping()
+  @PreAuthorize("hasRole('ROLE_provider')")
   public ResponseEntity<Bill> saveBill(@RequestBody Bill bill) {
     return ResponseEntity.ok().body(service.saveBill(bill));
   }
 
   @GetMapping("/findBy")
+  @PreAuthorize("hasRole('ROLE_provider')")
   public ResponseEntity<Bill> findByCustomer(@RequestParam String customer) {
     Bill bill = service.findByCustomer(customer);
     if (bill != null) {
