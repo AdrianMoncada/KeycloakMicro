a5d7b3ef225f199bb23ac5062758a1cf804e1d08
Beatriz Nieves <nievesb0429@gmail.com>
2023-03-19 17:55:50 -0500

    Agregando configuración de eureka

--- movies-api/src/main/java/com/digitalmedia/movies/MoviesApiApplication.java
+++ movies-api/src/main/java/com/digitalmedia/movies/MoviesApiApplication.java
@@ -2,8 +2,11 @@ package com.digitalmedia.movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MoviesApiApplication {
 
     public static void main(String[] args) {
